ec4e081dd517c7750f6241bc5ea4cd4d00beec38
Ayush-Mahajan <mail4ayushmahajan@gmail.com>
2016-11-22 19:56:04 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main()
 {
-    cout << "" << endl;
-    cout << "" << endl;
-    cout << "" << endl;
-    cout << "" << endl;
+    cout << "Scientific calculator" << endl;
+    cout << "1) Basic maths calculations" << endl;
+    cout << "2) Trignometric calculations" << endl;
+    cout << "3) " << endl;
     cout << "" << endl;
     cout << "" << endl;
     cout << "" << endl;
