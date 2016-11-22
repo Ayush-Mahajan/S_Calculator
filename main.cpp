6622f3bf384101e19006dbf605d47620220cf59e
Ayush-Mahajan <mail4ayushmahajan@gmail.com>
2016-11-22 15:29:50 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#inlcude<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    return 0;
+}
