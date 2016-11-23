1b43914ab202a6273c215f0d8e2f93437ac0336c
Ayush-Mahajan <mail4ayushmahajan@gmail.com>
2016-11-23 13:17:13 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,25 @@ using namespace std;
 
 int main()
 {
+    int choice, choice1;
     cout << "Scientific calculator" << endl;
     cout << "1) Basic maths calculations" << endl;
-    cout << "2) Trignometric calculations" << endl;
-    cout << "3) " << endl;
+    cout << "2) Trignometric/Inverse trignometric calculations" << endl;
+    cout << "3) Logarithimic calculation" << endl;
+    cout << "4) Exponentional calculation" << endl;
+    cout << "5) " << endl;
     cout << "" << endl;
-    cout << "" << endl;
-    cout << "" << endl;
+    cout << "What do you want to do - ";
+    cin >> choice;
+    switch(chioce)
+    {
+        case 1: cout << "1)Add" << endl;
+                cout << "2)Subtract" << endl;
+                cout << "3)Multiply" << endl;
+                cout << "4)Divide";
+                cout << "what do you wantg to do - ";
+                cin >> chioce1;
+        case 2: cout << 
+    }
     return 0;
 }
