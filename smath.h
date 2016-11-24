4baa6b781ca63f0752a854da30a6a3928658a79d
Ayush-Mahajan <mail4ayushmahajan@gmail.com>
2016-11-24 16:38:09 +0530

    Create smath.h

---
+++ smath.h
@@ -0,0 +1,8 @@
+// header guard for the file. it prevent the header file from being included more than once in a program
+#ifndef SMATH_H
+#define SMATH_H
+
+fact(int n);
+
+#endif
+// end of the heaer guard
