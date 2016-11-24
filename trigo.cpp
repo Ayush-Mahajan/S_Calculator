5ef35c9cf6abca852249b5f8b9d12abb5c560dc3
Ayush-Mahajan <mail4ayushmahajan@gmail.com>
2016-11-24 16:56:02 +0530

    Update and rename trigo.h to trigo.cpp

---
+++ trigo.cpp
@@ -0,0 +1,77 @@
+/* will have the code for all the trigo functions(sin, cos, tan, cot, sec, cosec,).It wil also have the code for inverse trigo functions. 
+By Shubhangi */
+
+#include<iostream>
+#include"smath.h"
+class trigo
+{
+    private: float angle, sum, value;
+             int x, fact;
+          
+    public: 
+        int sin(angle)
+        {
+            return 0;
+        }
+    
+        int cos(angle)
+        {
+            return 0;
+        }
+    
+        int tan(angle)
+        {
+            return 0;
+        }
+    
+        int cot(angle)
+        {
+            return 0;
+        } 
+    
+        int sec(angle)
+        {
+            return 0;   
+        }
+    
+        int cosec(angle)
+        {
+            return 0;    
+        }
+    
+        int i_sin(value)
+        {
+            return 0;    
+        }
+    
+        int i_cos(value)
+        {
+            return 0;    
+        }
+    
+        int i_tan(value)
+        {
+            return 0;    
+        }
+    
+        int i_cot(value)
+        {
+            return 0;    
+        }
+        int i_sec(value)
+        {
+            return 0;    
+        }
+    
+        int i_cosec(value)
+        {
+            return 0;    
+        }
+    
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
--- trigo.h
+++
@@ -1,5 +0,0 @@
-/* will have the code for all the trigo functions(sin, cos, tan, cot, sec, cosec,).It wil also have the code for inverse trigo functions. 
-By Shubhangi */
-
-#include<iostream>
-
