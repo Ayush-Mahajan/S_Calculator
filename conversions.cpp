9e9afe9ca9a25fb42d06da3d68540aadf028bc3c
Ayush-Mahajan <mail4ayushmahajan@gmail.com>
2016-11-25 11:23:27 +0530

    Create conversions.cpp

---
+++ conversions.cpp
@@ -0,0 +1,153 @@
+#include<iostream>
+
+using namespace std;
+//////////////////////////////////////////////////////////////////////////////////
+class angle
+{
+    private:
+        float degree, radian, gradian;
+        
+    public:
+        void DR() // Degree <-> Radian
+        {
+        
+        }
+        
+        void DG() //Degree <-> Gradians
+        {
+        
+        }
+        
+        void RG() //Radians <-> Gradians
+        {
+        
+        }
+}
+
+class AstUnit
+{
+    
+}
+
+class CompUnit
+{
+    private :
+        float bin, ter, quat, octal, hexad;
+        
+    public: 
+        void BT() // Binary <-> Decimal
+        {
+        
+        }
+        
+        void BQ() // Binary <-> Ternary
+        {
+        
+        }
+        
+        void BO() // Binary <-> Quaternary
+        {
+        
+        }
+        
+        void BH() // Binary <-> Octal
+        {
+        
+        }
+        
+        void TQ()
+        {
+        
+        }
+        
+        void TO()
+        {
+        
+        }
+        
+        void TH()
+        {
+        
+        }
+        
+        void QO()
+        {
+        
+        }
+        
+        void QH()
+        {
+        
+        }
+        
+        void OH()
+        {
+        
+        }
+    
+}
+
+class energy
+{
+
+}
+
+class force
+{
+
+}
+
+class frequency
+{
+
+}
+
+class heat
+{
+
+}
+
+class length
+{
+
+}
+
+class pressure
+{
+
+}
+
+class power
+{
+
+}
+
+class speed
+{
+
+}
+
+class time
+{
+
+}
+
+class volume
+{
+
+}
+
+class weight
+{
+
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////END OF CLASS DECLARATION/////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int 
+}
